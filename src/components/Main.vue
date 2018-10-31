83f5b377a00b9a875d6391e94dfcaa0baff1c108
Marianna <marianna.verissimo@assert.ifpb.edu.br>
2018-10-31 13:06:43 -0300

    Commit initial

---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
